8cecf43a9379e43444d59bbe469957fb6135031e
Noah Rollins <noahjrollins@gmail.com>
2022-12-19 15:03:26 -0600

    commit tree 87fa69b5ac5cfac26f058e53b99ee270a29708c4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    validate_triangle
+    if side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [side_a, side_b, side_c].all?(&:positive?)
+  end
+
+  def validate_triangle_inequality?
+    side_a + side_b > side_c && side_a + side_c > side_b && side_b + side_c > side_a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && validate_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
